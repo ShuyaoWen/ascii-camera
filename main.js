1134ea53311493575ef85b3aea77469a99f24546
Shuyao Wen <wenshuyao@hotmail.com>
2020-07-01 13:12:27 -0700

    added the ability to change resolution

--- main.js
+++ main.js
@@ -4,8 +4,10 @@ const button = document.getElementById("button");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const asciiImage = document.getElementById("ascii-image");
-let width = 256;
-let height = 144;
+const resolutionSelect = document.getElementById("resolution");
+let desiredWidth;
+let width = 320;
+let height = 240;
 
 let frameRate = 30;
 // let characterRamp = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ";
@@ -13,14 +15,14 @@ let frameRate = 30;
 let characterRamp = "@%#*+=-:. ";
 
 // takes rgb values and returns the grayscale value
-function getGrayscaleValue(r, g, b){
+function getGrayscaleValue(r, g, b) {
     return 0.3 * r + 0.59 * g + 0.11 * b;
 }
 
 // given a grayscale value, get it's equvalent character
-function getCharacter(grayscaleValue){
+function getCharacter(grayscaleValue) {
     // a grayscale value of 0 is black, a grayscale value of 255 is white
-    if(grayscaleValue === 255){
+    if (grayscaleValue === 255) {
         return characterRamp[characterRamp.length - 1];
     }
     let charIndex = Math.floor(grayscaleValue * characterRamp.length / 255);
@@ -29,61 +31,72 @@ function getCharacter(grayscaleValue){
 }
 
 // returns the eqivalent ascii art of the image
-function getAscii(){
+function getAscii() {
     let imageData = ctx.getImageData(0, 0, width, height);
     let asciiText = "";
     // the image data values for each pixel are in groups of 4. Values in the group are r, g, b ,a values.
     // this loop computes the grayscale values for all pixels
-    for(let i = 0; i < imageData.data.length; i += 4){
+    for (let i = 0; i < imageData.data.length; i += 4) {
         let newLine = width * 4;
         // at the end of each line, add a newline character
-        if(i % newLine === 0 && i !== 0){
+        if (i % newLine === 0 && i !== 0) {
             asciiText += "\n";
         }
         // get grascale value
-        let grayscaleValue = getGrayscaleValue(imageData.data[i], imageData.data[i+1], imageData.data[i+2]);
+        let grayscaleValue = getGrayscaleValue(imageData.data[i], imageData.data[i + 1], imageData.data[i + 2]);
         // get character value
         let char = getCharacter(grayscaleValue);
         // add character to ascii string
         asciiText += char;
 
     }
-    
+
     return asciiText;
 }
 
 // function to produce grayscale and ascii images
 function produceImage() {
+    let resOption = resolutionSelect.value.split("x");
+    width = resOption[0];
+    height = resOption[1];
     canvas.width = width;
     canvas.height = height;
+    let fontSize = 500 / height;
+    fontSize = fontSize.toString() + "px";
+    asciiImage.style.fontSize = fontSize;
     ctx.drawImage(video, 0, 0, width, height);
     asciiImage.textContent = getAscii();
-    
+
 }
 
 // start ascii camera function
-function startVideo(){
-    interval = setInterval(produceImage, 1000/frameRate);
+function startVideo() {
+    interval = setInterval(produceImage, 1000 / frameRate);
 }
 
 // pause ascii camera function
-function pauseVideo(){
+function pauseVideo() {
     clearInterval(interval);
 }
 
 // get video stream
 navigator.mediaDevices.getUserMedia({ video: true }).then(mediaStream => {
     video.srcObject = mediaStream;
+
 }).catch(error => console.log(error));
 
-button.addEventListener("click", function(){
-    if(button.value === "start video"){
-        console.log("video started");
+
+
+button.addEventListener("click", function () {
+    if (button.value === "start video") {        
+        startVideo();
+        button.value = "pause video";
+    } else if (button.value === "resume video") {
         startVideo();
         button.value = "pause video";
     } else {
         pauseVideo();
-        button.value = "start video";
+        button.value = "resume video";
     }
 
 });
